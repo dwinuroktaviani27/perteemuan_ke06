59da620637f834d361c65d12e56252f9e2932b8d
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2022-03-29 22:52:39 +0700

    Tampil Actions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import MainReducer from './reducers/MainReducer'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
+import { compose, createStore } from "redux";
+import MainReducer from "./reducers/MainReducer";
+import CreateTodo from "./containers/CreateTodo";
+import Table from "./containers/Table";
 
-const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+)(createStore)(MainReducer);
 
-ReactDOM.render(<Provider store={store}>
-  <App />
-</Provider>
-  , document.getElementById('root')); 
-  // registerServiceWorker(); 
+ReactDOM.render(
+  <Provider store={store}>
+    <CreateTodo />
+    <Table />
+  </Provider>,
+  document.getElementById("root")
+);
+// serviceWorker.unregister();
